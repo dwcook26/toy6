e1d882eb38a797e8bc47c2c6e64fb61b38500049
That Guy <dwcook26@students.randolphschool.net>
2024-04-26 17:21:16 -0500

    fix2

--- builds/index.service.worker.js
+++ builds/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1714169732|30931742";
+const CACHE_VERSION = "1714170070|368940687";
 const CACHE_PREFIX = "GOA1-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
